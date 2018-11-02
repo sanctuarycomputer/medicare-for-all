c339d56bcdf65ae90dbb9d603607d08e89c4aceb
sohee Lim <limsohee1002@gmail.com>
2018-11-02 17:30:25 -0400

    change background image to be generated from css

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,9 +5,11 @@ import styles from './Hero.scss';
 class App extends Component {
   render() {
     return (
-      <div className={cx(styles['Hero'], 'vh100 vw100 relative p4 flex flex-col items-center')}>
-        <div className={cx(styles['Hero__background-image'], 'col-10 h100 absolute t0')}/>
-        <div className='absolute t0 w100 h100 p4 flex flex-col items-center justify-center'>
+      <div className={cx(styles['Hero'], 'vw100 vh100 relative flex flex-col items-center')}>
+        <div className={(styles['Hero__circle-container'], 'w100 h100 absolute flex items-center justify-center p2')}>
+          <div className={cx(styles['Hero__circle'], 'w100')}/>
+        </div>
+        <div className='absolute t0 w100 h100 flex flex-col items-center justify-center'>
           <img className='col-10 my4' src='/assets/image-medicare.svg' alt='medicare'/>
           <img className='col-2 none lg:block' src='/assets/sanctu-compu-logo.svg' alt='sanctuary computer logo'/>
           <img className='col-6 my4' src='/assets/image-for-all.svg' alt='for all'/>
